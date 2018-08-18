f0b0e57aea62c3362a16260655e178091f59199e
Joshua Qua Hiansen <jquahian@gmail.com>
2018-08-18 17:01:52 -0400

    login screen style updated, radio button behaviour uptaded

---
+++ test.js
@@ -0,0 +1,25 @@
+var test = function(){
+	alert("test");
+}
+
+var foo = function(a){
+	alert(a);
+}
+
+var num = function(b){
+	var good = b;
+	alert(b);
+}
+
+var forLoop = function(){
+	for(var i = 0; i <=10; i++){
+		console.log(i);
+	}
+}
+
+var forEachLoop = function(){
+	var nums = [1231,233,35,4123,512];
+	for(var i = 0; i < nums.length; i++){
+		console.log(nums[i]);
+	}
+}
